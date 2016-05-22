9450450bb38c3c40c5f788b05a83a963b19f8640
algebur <algebur@gmail.com>
2016-05-22 14:50:26 -0600

    lya-lya-lya

---
+++ layout/scripts/basicscripts.js
@@ -0,0 +1,6 @@
+    function divideNumbers(){
+       var f = prompt("Enter the first number");
+       var g = prompt("Enter the second number");
+       alert("The divident of numbers is " + f/g);
+    }
+    
